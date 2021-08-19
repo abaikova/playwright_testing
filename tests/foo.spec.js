c55518307d24501eb9ee616efdc6bf49ddc755e3
Alina Baikova <abaikova@ss8.com>
2021-08-19 17:20:08 +0300

    get started with the playwright. the first test

---
+++ tests/foo.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@playwright/test');
+
+test('basic test', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    const title = page.locator('.navbar__inner .navbar__title');
+    await expect(title).toHaveText('Playwright');
+
+    await expect(page).toHaveTitle('Playwright');
+
+    await expect(page.locator('text=Get Started')).toHaveAttribute('href', '/docs/intro');
+});
