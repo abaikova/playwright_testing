6e37323b4115008299956b75fe5b102582c35dc7
Alina <lina.baikova@gmail.com>
2021-09-08 15:30:12 +0300

    update configs

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -1,12 +1,14 @@
 const config = {
+    timeout: 30000,
     retries: 1,
     reporter: [
         ['list'],
         ['experimental-allure-playwright']
     ],
     use: {
+        ignoreHTTPSErrors: true,
         browserName: 'chromium',
-        headless: false,
+        // headless: false,
         screenshot: 'only-on-failure',
     },
 };
